e8fbb64cf95e93b0adc52c7a2d9d54775123911f
Murad4444 <sh.murad.hsl@gmail.com>
2025-07-25 16:11:51 +0000

    Fix: Update pubspec.yaml and firebase versions

---
+++ my_new_app/android/build.gradle.kts
@@ -0,0 +1,38 @@
+// android/build.gradle.kts
+
+buildscript {
+    val kotlin_version by extra("1.8.20") // <-- Здесь правильно
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version") // <-- ИСПРАВЛЕНО ЗДЕСЬ!
+        classpath("com.google.gms:google-services:4.4.1")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
+
+
